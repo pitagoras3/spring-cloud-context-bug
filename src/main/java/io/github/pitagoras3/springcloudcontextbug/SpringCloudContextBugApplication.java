7f4fbcf022ff10bb7bfb218574e82ad5d76f11d6
Szymon Marcinkiewicz <szymon.a.marcinkiewicz@allegro.pl>
2023-03-20 10:51:31 +0100

    Initialized a project

---
+++ src/main/java/io/github/pitagoras3/springcloudcontextbug/SpringCloudContextBugApplication.java
@@ -0,0 +1,13 @@
+package io.github.pitagoras3.springcloudcontextbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContextBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudContextBugApplication.class, args);
+    }
+
+}
